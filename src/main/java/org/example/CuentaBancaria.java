77c1d6e225fdd5105ad18e30c8b1004e67799f30
alopezalbares <antonio.lopez98@educa.madrid.org>
2025-04-19 10:26:32 +0200

    Cambio rama master repositorio IJ

--- src/main/java/org/example/CuentaBancaria.java
+++ src/main/java/org/example/CuentaBancaria.java
@@ -22,7 +22,7 @@ public class CuentaBancaria {
      */
     public void depositar(double cantidad) {
         if (cantidad > 0) {
-            saldo += cantidad; // Cambio en EjemploRamaEnGH
+            saldo += cantidad; // Cambio rama master repositorio IJ
         }
     }
 
